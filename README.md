79471e68c69fb9d34e5dcc7b46bef7bc76b61dff
0xLit <104043008+0xlit@users.noreply.github.com>
2022-10-12 18:49:33 +0300

    readme

---
+++ README.md
@@ -0,0 +1,30 @@
+## Setup
+
+NodeJS is required
+
+```
+npm install
+```
+
+## Environment Variable
+
+File '.env' need to be defined before running
+
+```
+// PrivateKeys to be used for minting
+PRIVATE_KEY1=
+PRIVATE_KEY2=
+PRIVATE_KEY3=
+
+// Json-RPC provider address(websocket address is required because of events)
+JSON_RPC1=
+
+// Mint contract address on chain
+LAUNCHPEG_ADDRESS=
+```
+
+## Run
+
+```
+node index.js
+```
--- index.js
+++ index.js
@@ -2,21 +2,22 @@ const Web3 = require("web3");
 const {Worker, workerData, isMainThread, parentPort} = require("worker_threads");
 require("dotenv").config();
 
-const rpcs = [process.env.RPC1];
-let webSocketIP = rpcs[0];
+// JsonRPC IP
+const jsonRPCs = [process.env.JSON_RPC1];
+const webSocketIP = jsonRPCs[0];
 
-// private keys of minter addresses
-const privateKeys = [process.env.KEY1, process.env.KEY2, process.env.KEY3];
+// Private keys of minter addresses
+const privateKeys = [process.env.PRIVATE_KEY1, process.env.PRIVATE_KEY2, process.env.PRIVATE_KEY3];
 
-// Launchpeg on-chain
-const LaunchpegAddress = "0xc60d7980760218f890a62d3e99dd1a884124d9d6";
+// Launchpeg minting address on chain
+const LaunchpegAddress = process.env.LAUNCHPEG_ADDRESS;
 
 // Launchpeg ABI
 const LaunchpegABI = require("./abi/LaunchpegABI.json");
 
 async function main() {
   try {
-    // webSocket provider with configurations
+    // WebSocket provider with configurations
     const webSocketProvider = await new Web3.providers.WebsocketProvider(webSocketIP, {
       clientConfig: {
         keepalive: true,
@@ -45,11 +46,11 @@ async function main() {
           // Logs
           console.log(`Launchpeg Initialized`);
 
-          //Get Start Time
+          //Get Start Time from event response
           let startTime = response.returnValues.allowlistStartTime;
           console.log(`Set Mint Time ${startTime}`);
 
-          //Launchpeg Initialized() event emmitted spawning threads
+          //Launchpeg Initialized() event emmitted, spawning workers
           const worker1 = new Worker("./index.js", {
             workerData: {numThreads: 0, startTime: startTime},
           });
@@ -60,7 +61,7 @@ async function main() {
             workerData: {numThreads: 2, startTime: startTime},
           });
 
-          // Returning Logs From Workers
+          // Returning logs from workers
           worker1.on("message", (res) => {
             console.log(res);
           });
@@ -72,18 +73,18 @@ async function main() {
           });
         })
         .on("error", (error, receipt) => {
-          // Logs Error
+          // Error logs
           console.log(`Initialized() event ${error}`);
         });
 
       // Logs
       console.log(`Waiting for Initialized() Event`);
     }
-    // if not the maint thread
+    // If not the maint thread
     else {
       parentPort.postMessage(`Spawned Worker ${workerData.numThreads + 1} is Waiting For Mint Time`);
 
-      // Initialize Account from privateKeytry
+      // Initialize Account from privateKey
       account = await web3.eth.accounts.privateKeyToAccount(privateKeys[workerData.numThreads]);
 
       // Add account to wallet
@@ -99,7 +100,7 @@ async function main() {
           // Logs
           parentPort.postMessage(`Sending Mint TX for Wallet No: ${workerData.numThreads}`);
 
-          // Send Mint Transactions
+          // Send mint transaction
           try {
             let mintTx = await LaunchpegContract.methods.allowlistMint(1).send({
               gas: 300000,
@@ -111,16 +112,17 @@ async function main() {
             // Logs
             parentPort.postMessage("Mint Tx has succesfully sent");
 
-            //Stop trying if mint success
+            //Stop trying if mint is success
             keepTrying = false;
           } catch (err) {
-            // Error Logs
+            // Error logs
             parentPort.postMessage(`Send Mint TX ${err}`);
           }
         }
       }
     }
   } catch (err) {
+    // Error logs
     console.log(`Main ${err}`);
   }
 }
